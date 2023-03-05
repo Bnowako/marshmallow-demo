d995f5e7b70038a3c0ae6321c1a69a5b9339a662
Bnowako <blazejnowakowski98@gmail.com>
2023-03-05 19:23:03 +0100

    first commit

---
+++ src/main/kotlin/bnowako/marshmallowdemo/MarshmallowDemoApplication.kt
@@ -0,0 +1,11 @@
+package bnowako.marshmallowdemo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MarshmallowDemoApplication
+
+fun main(args: Array<String>) {
+    runApplication<MarshmallowDemoApplication>(*args)
+}
---
+++ src/test/kotlin/bnowako/marshmallowdemo/MarshmallowDemoApplicationTests.kt
@@ -0,0 +1,13 @@
+package bnowako.marshmallowdemo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MarshmallowDemoApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
